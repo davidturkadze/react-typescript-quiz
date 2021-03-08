8f67f05f4d023767a8dea58940c583058b7271ae
David Turkadze <nebel89@gmail.com>
2021-03-08 14:51:24 +0100

    fix bg-img on android

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -9,6 +9,7 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background-image: url(${BGImage});
     background-size: cover;
+    background-repeat:no-repeat;
     margin: 0;
     display: flex;
     justify-content: center;
